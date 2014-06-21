26fe00f03551b162a53dde09fdf71804d1149ab0
SaadSurya <saadsurya@hotmail.com>
2014-06-21 20:49:30 +0500

    Member's Master Done

--- DB Script/lolism_contact.sql
+++ DB Script/lolism_contact.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jun 21, 2014 at 01:52 PM
+-- Generation Time: Jun 21, 2014 at 03:48 PM
 -- Server version: 5.5.24-log
 -- PHP Version: 5.3.13
 
@@ -28,18 +28,18 @@ SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `company` (
   `CompanyID` int(11) NOT NULL AUTO_INCREMENT,
-  `CompanyName` varchar(300) NOT NULL,
-  `IndustoryCategory` int(11) NOT NULL,
-  `IndustorySubCategory` int(11) NOT NULL,
-  `Fax` varchar(20) NOT NULL,
-  `City` varchar(200) NOT NULL,
-  `State` varchar(200) NOT NULL,
-  `ZipCode` int(11) NOT NULL,
-  `Country` int(11) NOT NULL,
-  `Website` varchar(50) NOT NULL,
-  `Remarks` varchar(1000) NOT NULL,
+  `CompanyName` varchar(300) DEFAULT NULL,
+  `IndustoryCategory` int(11) DEFAULT NULL,
+  `IndustorySubCategory` int(11) DEFAULT NULL,
+  `Fax` varchar(20) DEFAULT NULL,
+  `City` varchar(200) DEFAULT NULL,
+  `State` varchar(200) DEFAULT NULL,
+  `ZipCode` int(11) DEFAULT NULL,
+  `Country` int(11) DEFAULT NULL,
+  `Website` varchar(50) DEFAULT NULL,
+  `Remarks` varchar(1000) DEFAULT NULL,
   PRIMARY KEY (`CompanyID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=17 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=22 ;
 
 --
 -- Dumping data for table `company`
@@ -61,7 +61,12 @@ INSERT INTO `company` (`CompanyID`, `CompanyName`, `IndustoryCategory`, `Industo
 (13, 'Test1', 36, 64, '1234567890', 'Karachi', 'Sindh', 74000, 1, 'company.com', 'qwerty'),
 (14, 'Test1', 36, 74, '1234567890', 'Karachi', 'Sindh', 74000, 1, 'company.com', 'qwery'),
 (15, 'Test1', 36, 74, '1234567890', 'Karachi', 'Sindh', 74000, 1, 'company.com', 'qwery'),
-(16, 'Test1', 36, 74, '1234567890', 'Karachi', 'Sindh', 74000, 1, 'company.com', 'qwery');
+(16, 'Test1', 36, 74, '1234567890', 'Karachi', 'Sindh', 74000, 1, 'company.com', 'qwery'),
+(17, 'Test2', 36, 74, '1234567890', 'Karachi', 'Sindh', 74000, 2, 'company.com', 'qwerty'),
+(18, 'Test2', 36, 74, '1234567890', 'Karachi', 'Sindh', 74000, 2, 'company.com', 'qwerty'),
+(19, 'asdf', 1, 1, '123', 'Karachi', 'Sindh', 74000, 2, 'asdf.com', 'asdfadsf'),
+(20, 'Test2', 36, 74, '1234567890', 'Karachi', 'Sindh', 74000, 2, 'company.com', 'qwerty'),
+(21, 'Test1', 46, 64, '1234567890', 'Karachi', 'Sindh', 74000, 2, 'company.com', 'qwert');
 
 -- --------------------------------------------------------
 
@@ -72,12 +77,12 @@ INSERT INTO `company` (`CompanyID`, `CompanyName`, `IndustoryCategory`, `Industo
 CREATE TABLE IF NOT EXISTS `companyaddress` (
   `CompanyAddressID` int(11) NOT NULL AUTO_INCREMENT,
   `CompanyID` int(11) NOT NULL,
-  `Address` varchar(100) NOT NULL,
-  `Phone` varchar(20) NOT NULL,
-  `Email` varchar(50) NOT NULL,
+  `Address` varchar(100) DEFAULT NULL,
+  `Phone` varchar(20) DEFAULT NULL,
+  `Email` varchar(50) DEFAULT NULL,
   `IsPrimary` tinyint(1) NOT NULL,
   PRIMARY KEY (`CompanyAddressID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10 ;
 
 --
 -- Dumping data for table `companyaddress`
@@ -89,7 +94,43 @@ INSERT INTO `companyaddress` (`CompanyAddressID`, `CompanyID`, `Address`, `Phone
 (3, 0, 'Add3', '3', 'test@test.com', 0),
 (4, 0, 'Add4', '4', 'test@test.com', 0),
 (5, 0, 'Add1', '1', 'test@test.com', 1),
-(6, 0, 'Add2', '2', 'test@test.com', 0);
+(6, 0, 'Add2', '2', 'test@test.com', 0),
+(7, 20, 'Add1', '1', 'test@test.com', 0),
+(8, 20, 'Add2', '2', 'test@test.com', 1),
+(9, 21, 'Add1', '1', 'test@test.com', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `member`
+--
+
+CREATE TABLE IF NOT EXISTS `member` (
+  `MemberID` int(11) NOT NULL AUTO_INCREMENT,
+  `Title` varchar(10) DEFAULT NULL,
+  `FirstName` varchar(200) DEFAULT NULL,
+  `MiddleName` varchar(200) DEFAULT NULL,
+  `LastName` varchar(200) DEFAULT NULL,
+  `Email` varchar(50) DEFAULT NULL,
+  `CellPhone` varchar(20) DEFAULT NULL,
+  `OfficePhone` varchar(20) DEFAULT NULL,
+  `Phone` varchar(20) DEFAULT NULL,
+  `Fax` varchar(20) DEFAULT NULL,
+  `Address` varchar(1000) DEFAULT NULL,
+  `City` varchar(200) DEFAULT NULL,
+  `State` varchar(200) DEFAULT NULL,
+  `ZipCode` int(11) DEFAULT NULL,
+  `Country` int(11) DEFAULT NULL,
+  `Remarks` varchar(1000) DEFAULT NULL,
+  PRIMARY KEY (`MemberID`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `member`
+--
+
+INSERT INTO `member` (`MemberID`, `Title`, `FirstName`, `MiddleName`, `LastName`, `Email`, `CellPhone`, `OfficePhone`, `Phone`, `Fax`, `Address`, `City`, `State`, `ZipCode`, `Country`, `Remarks`) VALUES
+(1, 'Ms', 'First', 'Middle', 'Last', 'qwer@qwer.com', '132465', '123456', '132456', '1234567890', 'Home Address', 'Karachi', 'Sindh', 74000, 2, 'qwerty');
 
 -- --------------------------------------------------------
 
